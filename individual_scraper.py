17282b9a9d8333d37e950fdafa7500af6037a48a
Devesh Kashyap <kashyapdevesh30@gmail.com>
2022-05-22 03:59:18 +0530

    Single instance scraper completed

--- individual_scraper.py
+++ individual_scraper.py
@@ -4,21 +4,25 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 def render_page(url):
         firefox_options = Options()
-        firefox_options.add_argument('--headless')
+        #firefox_options.add_argument('--headless')
         driver=webdriver.Firefox(options=firefox_options)
         driver.get(url)
         scrape_summary(driver.page_source) #scrape basic info from top panel
         time.sleep(2)
-        iframeElement = driver.find_element(by=By.TAG_NAME, value='iframe') #change driver to iframe tag for scraping comments
-        driver.switch_to.frame(iframeElement)
+        WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.TAG_NAME,"iframe")))
+        load_all_comments(driver) # open all hidden comments
         r = driver.page_source
         driver.quit()
         return r
   
+
       
 def scrape_comments(all_comments):
 	final_comments=[]
@@ -50,10 +54,24 @@ def scrape_summary(page_source):
 	manga_desc=soup.find_all("div",{"class":"panel-story-info-description"})[0].text[14:]
 	final_summary={"Manga's Name":manga_name,"Manga's cover Image":manga_img,"Manga's author(s)":manga_author,"Manga's current status": manga_status,"Manga's genre(s)":manga_genre,"Manga rating":manga_rating,"Manga description":manga_desc}
 	print(final_summary)
+	#return final_summary
 	
-	
-				
-
+def load_all_comments(driver):
+	print("\n\n\n LOADING COMMENTS \n\n\n")
+	driver.switch_to.default_content()
+	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+	WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.TAG_NAME,"iframe")))
+	driver.find_element(by=By.XPATH, value="//button[@class='_1gl3 _4jy0 _4jy3 _517h _51sy _42ft']").click()
+	while True:
+		try :
+			driver.switch_to.default_content()
+			driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+			print("Scrolled Down")
+			WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.TAG_NAME,"iframe")))
+			WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@class='_1gl3 _4jy0 _4jy3 _517h _51sy _42ft']"))).click()
+		except :
+			print("NO COMMENTS LEFT")
+			break
 
 def manga_scraper(url):
     r = render_page(url)
